ac6019d736ef17a98fbc698a15b1d293e3693a94
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-03-07 09:46:57 -0300

    ✨  Split slide by --- and use arrows to jump page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
 console.log("app.js", "Hello World!");
+
+class App {
+  constructor() {
+    let body = document.getElementsByTagName("body")[0];
+    let pre = body.getElementsByTagName("pre")[0];
+    let el = pre || body;
+    let page = 1;
+
+    this.state = {
+      el: el,
+      page: page,
+      slides: el.innerHTML.split("---").map(s => s.trim())
+    }
+  }
+  init() {
+    this.listenKeyPresses();
+    this.render();
+  }
+  listenKeyPresses() {
+    window.addEventListener("keydown", e => {
+      let {page, slides} = this.state;
+
+      if (e.key === "ArrowRight" && page < slides.length){
+        this.setState("page", page + 1);
+      }
+      if (e.key === "ArrowLeft" && page > 1){
+        this.setState("page", page - 1);
+      }
+    });
+  }
+  setState(key, value) {
+    this.state[key] = value;
+    this.render();
+  }
+  render() {
+    let {el, slides, page} = this.state;
+    el.innerHTML = slides[page - 1];
+  }
+}
+
+window.onload = () => new App().init();
--- js/background.js
+++ js/background.js
@@ -3,7 +3,11 @@ console.log("[background.js]", "Hello World!");
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {
+          pathSuffix: "\.md"
+        }
+      })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
